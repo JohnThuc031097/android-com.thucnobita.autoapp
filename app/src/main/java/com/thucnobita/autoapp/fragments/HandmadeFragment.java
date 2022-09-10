f3e8485053b52bebabee0db152f1bdfed768f085
JohnThuc1997 <johnthuc1997@gmail.com>
2022-09-11 01:51:10 +0700

    Updating v3

---
+++ app/src/main/java/com/thucnobita/autoapp/fragments/HandmadeFragment.java
@@ -0,0 +1,32 @@
+package com.thucnobita.autoapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thucnobita.autoapp.R;
+
+public class HandmadeFragment extends Fragment {
+
+    public HandmadeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_handmade, container, false);
+    }
+}
